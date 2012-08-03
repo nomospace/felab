c7858f7c2ae3fff6f382cd8cc8d3804d4d5e7cad
nomospace <jinlu_hz@163.com>
2012-08-03 14:58:14 +0800

    add r.js

--- code/js-fragment/row-select.js
+++ code/js-fragment/row-select.js
@@ -1,19 +1,20 @@
 /**
  * 单击表格行任意处选中单选、复选
+ * @author: jinlu_hz@163.com
  * @date: 2012-3-1
  */
 (function() {
-  $('body').on('click', 'tbody tr', function(e) {
-    var target = e.target,
-      type = target.type,
-      tagName = target.tagName.toLocaleLowerCase(),
-      $input;
+    $('body').on('click', 'tbody tr', function(e) {
+        var target = e.target,
+            type = target.type,
+            tagName = target.tagName.toLocaleLowerCase(),
+            $input;
 
-    if (type == 'checkbox' || type == 'radio' ||
-      tagName == 'select' || tagName == 'option' ||
-      tagName == 'input' || tagName == 'textarea') return;
+        if (type == 'checkbox' || type == 'radio' ||
+            tagName == 'select' || tagName == 'option' ||
+            tagName == 'input' || tagName == 'textarea') return;
 
-    $input = $(this).find(':checkbox,:radio');
-    $input.prop('checked', !$input.prop('checked'));
-  });
+        $input = $(this).find(':checkbox,:radio');
+        $input.prop('checked', !$input.prop('checked'));
+    });
 })();
--- code/js-fragment/scroll-fixed.js
+++ code/js-fragment/scroll-fixed.js
@@ -1,45 +1,46 @@
 /**
- * @author: jinlu
+ * 元素自动固定定位模块
+ * @author: jinlu_hz@163.com
  * @date: 2012-7-19
  */
 define(function() {
-  'use strict';
+    'use strict';
 
-  var fixedStyle = 'position:fixed;top:0;z-index:999;background-color:#fff;border:1px solid #ddd;border-top:0 none;border-left:0 none;',
-    $win = $(window),
-    ScrollFixed = function(options) {
-      this.$target = $(options.target);
-      this.targetOffsetTop = this.$target.offset().top;
-      this.targetHeight = this.$target.outerHeight();
-      this.$placeholder = $('<div style="height:0;"></div>');
-      this.$target.after(this.$placeholder);
-      fixedStyle += ';width:' + (this.$target.width() - 1) + 'px;';
-      $win.scroll(_.throttle($.proxy(this._scroll, this), 30));
+    var fixedStyle = 'position:fixed;top:0;z-index:999;background-color:#fff;border:1px solid #ddd;border-top:0 none;border-left:0 none;',
+        $win = $(window),
+        ScrollFixed = function(options) {
+            this.$target = $(options.target);
+            this.targetOffsetTop = this.$target.offset().top;
+            this.targetHeight = this.$target.outerHeight();
+            this.$placeholder = $('<div style="height:0;"></div>');
+            this.$target.after(this.$placeholder);
+            fixedStyle += ';width:' + (this.$target.width() - 1) + 'px;';
+            $win.scroll(_.throttle($.proxy(this._scroll, this), 30));
+        }
+
+    ScrollFixed.prototype = {
+        _scroll: function() {
+            this._shouldFixed() ? this._fixed() : this._unfixed();
+        },
+
+        _shouldFixed: function() {
+            return $win.scrollTop() >= this.targetOffsetTop;
+        },
+
+        _fixed: function() {
+            this.$target.attr('style', fixedStyle);
+            this.$placeholder.height(this.targetHeight + 'px');
+        },
+
+        _unfixed: function() {
+            this.$target.attr('style', 'position:relative');
+            this.$placeholder.height(0);
+        }
     }
 
-  ScrollFixed.prototype = {
-    _scroll: function() {
-      this._shouldFixed() ? this._fixed() : this._unfixed();
-    },
-
-    _shouldFixed: function() {
-      return $win.scrollTop() >= this.targetOffsetTop;
-    },
-
-    _fixed: function() {
-      this.$target.attr('style', fixedStyle);
-      this.$placeholder.height(this.targetHeight + 'px');
-    },
-
-    _unfixed: function() {
-      this.$target.attr('style', 'position:relative');
-      this.$placeholder.height(0);
+    return {
+        init: function(options) {
+            return new ScrollFixed(options);
+        }
     }
-  }
-
-  return {
-    init: function(options) {
-      return new ScrollFixed(options);
-    }
-  }
 });
