12018bfdc45793285f7b4ee2a2d28bcc300f702d
nomospace <jinlu_hz@163.com>
2012-08-03 14:45:29 +0800

    upload code fragment

---
+++ code/js-fragment/row-select.js
@@ -0,0 +1,19 @@
+/**
+ * 单击表格行任意处选中单选、复选
+ * @date: 2012-3-1
+ */
+(function() {
+  $('body').on('click', 'tbody tr', function(e) {
+    var target = e.target,
+      type = target.type,
+      tagName = target.tagName.toLocaleLowerCase(),
+      $input;
+
+    if (type == 'checkbox' || type == 'radio' ||
+      tagName == 'select' || tagName == 'option' ||
+      tagName == 'input' || tagName == 'textarea') return;
+
+    $input = $(this).find(':checkbox,:radio');
+    $input.prop('checked', !$input.prop('checked'));
+  });
+})();
---
+++ code/js-fragment/simple-taber.js
@@ -0,0 +1,42 @@
+/**
+ * 简易taber模块
+ * @author: jinlu.nomospace
+ * @date: 12-3-22
+ */
+define(function() {
+  var index = -1;    // 当前tab index
+
+  return {
+    // TODO index交给模块自己计算
+    /**
+     * 设置标签tabs
+     * @param list
+     * @param options
+     */
+    refreshTaber: function(list, options) {
+      var $list = $(list), // tab列表
+        selected = options.selected || 'selected', // tab选中时的样式
+        callback = options.callback;    // tab切换后的回调函数
+      index = options.index || 0; // 第index个tab
+
+      $.each($list, function(i, item) {
+        item = $(item);
+        if (i !== index) {
+          item.removeClass(selected);
+        }
+        else {
+          item.addClass(selected);
+        }
+      });
+
+      $.isFunction(callback) && callback();
+    },
+
+    /**
+     * 获取当前tab index
+     */
+    getIndex: function() {
+      return index;
+    }
+  }
+});
