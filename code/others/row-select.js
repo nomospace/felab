a6005e06fa842e09b77eba15f868ec2e1b2a7c13
nomospace <jinlu_hz@163.com>
2013-01-15 10:04:49 +0800

    Add oc module

--- code/others/row-select.js
+++ code/others/row-select.js
@@ -1,20 +1,27 @@
 /**
- * 单击表格行任意处选中单选、复选
- * @author: jinlu_hz@163.com
- * @date: 2012-3-1
+ * 单击表格行任意处选择[取消选择] checkbox[radio]
  */
 (function() {
-    $('body').on('click', 'tbody tr', function(e) {
-        var target = e.target,
-            type = target.type,
-            tagName = target.tagName.toLocaleLowerCase(),
-            $input;
-
-        if (type == 'checkbox' || type == 'radio' ||
-            tagName == 'select' || tagName == 'option' ||
-            tagName == 'input' || tagName == 'textarea') return;
-
-        $input = $(this).find(':checkbox,:radio');
-        $input.prop('checked', !$input.prop('checked'));
+  'use strict';
+  var klass = 'row-selected';
+  $('body').on('click', 'tbody tr',function(e) {
+    var target = e.target,
+      $this = $(this),
+      $input = $this.find('td > :checkbox,td > :radio');
+    if (_.indexOf(['checkbox', 'radio'], target.type) !== -1 ||
+      _.indexOf(['select', 'option' , 'input' , 'textarea', 'a'], target.tagName.toLocaleLowerCase()) !== -1) {
+      return;
+    }
+    if ($input.length) {
+      var checked = $input.prop('checked');
+      if ($input.attr('type') !== 'radio') {
+        checked ? $this.removeClass(klass) : $this.addClass(klass);
+      }
+      $input.prop('checked', !checked).change();
+    }
+  }).on('change', 'tbody tr :checkbox', function() {
+      var $cb = $(this),
+        $tr = $cb.closest('tr');
+      $cb.is(':checked') ? $tr.addClass(klass) : $tr.removeClass(klass);
     });
 })();
--- code/others/simple-taber.js
+++ code/others/simple-taber.js
@@ -1,42 +1,38 @@
 /**
- * 简易taber模块
- * @author: jinlu_hz@163.com
- * @date: 2012-3-22
+ * 简易 taber 模块
  */
 define(function() {
-    var index = -1;    // 当前tab index
-
-    return {
-        // TODO index交给模块自己计算
-        /**
-         * 设置标签tabs
-         * @param list
-         * @param options
-         */
-        refreshTaber: function(list, options) {
-            var $list = $(list), // tab列表
-                selected = options.selected || 'selected', // tab选中时的样式
-                callback = options.callback;    // tab切换后的回调函数
-            index = options.index || 0; // 第index个tab
-
-            $.each($list, function(i, item) {
-                item = $(item);
-                if (i !== index) {
-                    item.removeClass(selected);
-                }
-                else {
-                    item.addClass(selected);
-                }
-            });
-
-            $.isFunction(callback) && callback();
-        },
-
-        /**
-         * 获取当前tab index
-         */
-        getIndex: function() {
-            return index;
+  'use strict';
+  var index = -1;    // 当前 tab index
+  /**
+   * 设置标签 tabs
+   * @param list
+   * @param options
+   */
+  return {
+    // TODO index 交给模块自己计算
+    refreshTaber: function(list, options) {
+      var $list = $(list), // tab 列表
+        selected = options.selected || 'selected', // tab 选中时的样式
+        callback = options.callback;    // tab 切换后的回调函数
+      index = options.index || 0; // 第 index 个 tab
+      $.each($list, function(i, item) {
+        item = $(item);
+        if (i !== index) {
+          item.removeClass(selected);
         }
+        else {
+          item.addClass(selected);
+        }
+      });
+      $.isFunction(callback) && callback();
+    },
+
+    /**
+     * 获取当前tab index
+     */
+    getIndex: function() {
+      return index;
     }
+  }
 });
